ec7c68341e5047334e4345532f98cf68e8f4b103
Maxim Guriev <maxim.guriev@yandex.ru>
2023-12-04 19:40:36 +0500

    Add new exception

--- test_main_page.py
+++ test_main_page.py
@@ -1,6 +1,7 @@
 import pytest
 from selenium import webdriver
 from .pages.main_page import MainPage
+from selenium.common.exceptions import NoSuchElementException
 import time
 
 def test_guest_should_see_login_link(browser):
@@ -8,3 +9,9 @@ def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
     page.open()                      # открываем страницу
     page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логина
+
+def test_guest_should_see_login_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
+    page.open()
+    page.should_be_login_link()
